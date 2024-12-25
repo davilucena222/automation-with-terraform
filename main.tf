710cb7fca6a0fb264fdcb4cd76b49e53a1c8ae3c
Davi Luiz <davilucena222@gmail.com>
2024-12-24 21:48:59 -0300

    providing settings with ansible files

--- main.tf
+++ main.tf
@@ -17,8 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0e2c8caa4b6378d8c" # ubuntu server image 
   instance_type = "t2.micro"
   key_name = "iac-aws-terraform"
-
+  # user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Deployed via Terraform IaC</h1>" > index.html
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
   tags = {
-    Name = "AppServerInstance"
+    Name = "AWS VM with Python and Virtualenv"
   }
 }
